6ef9a25109135d8a4734a209c8920955b82591f9
arthur.jourdan <arthur.jourdan@epitech.eu>
2020-05-24 20:17:05 +0200

    refactor(check_fill_aliases.c): change the way aliases are substituted for more precision and to be able to do alias of alias

--- src/built_ins/aliases/check_fill_aliases.c
+++ src/built_ins/aliases/check_fill_aliases.c
@@ -10,37 +10,51 @@
 
 #include "built_in.h"
 
-static char *replace_with_substitute(char *line, char *word, alias_t *alias)
+static char *replace_with_substitute(char *line, alias_t *alias, ssize_t pos)
 {
-    char *new = NULL;
-    ssize_t after_word = get_pos_word_end_in_str(word, line);
+    char *tmp_end = NULL;
+    char *tmp_beg = NULL;
+    size_t len_alias = my_strlen(alias->alias);
 
-    if (after_word == -1)
-        return line;
-    new = my_strcat(alias->substitute, line + after_word, false, false);
-    free_char_to_null(line);
-    return new;
+    tmp_end = my_strcpy(line + pos + len_alias);
+    tmp_beg = my_str_n_cpy(line, pos);
+    // line = free_char_to_null(line);
+    line = my_strcat_tot(3, tmp_beg, alias->substitute, tmp_end);
+    free(tmp_end);
+    free(tmp_beg);
+    return line;
 }
 
 static char *check_for_alias(char *line, alias_t *alias)
 {
-    char **tmp_line = tmp_line = my_str_to_word_arr(line);
+    ssize_t pos = get_pos_word_in_str(alias->alias, line);
 
-    if (!tmp_line)
-        return line;
-    if (my_strcmp(tmp_line[0], alias->alias)) {
-        line = replace_with_substitute(line, tmp_line[0], alias);
+    while (pos != -1) {
+        line = replace_with_substitute(line, alias, pos);
+        pos += my_strlen(alias->alias);
+        if (get_pos_word_in_str(alias->alias, line + pos) == -1)
+            break;
+        pos += get_pos_word_in_str(alias->alias, line + pos);
     }
-    free_double_char_arr(tmp_line);
     return line;
 }
 
 char *check_fill_aliases(char *line, alias_t **aliases)
 {
+    ssize_t pos_alias_word = 0;
+    bool alias_of_alias = false;
+
     if (!line || !aliases)
         return line;
+    pos_alias_word = get_pos_word_in_str("alias", line);
+    if (pos_alias_word == 0) {
+        alias_of_alias = true;
+        line += 5;
+    }
     for (size_t a = 0; aliases[a]; a++) {
         line = check_for_alias(line, aliases[a]);
     }
+    if (alias_of_alias)
+    line = my_strcat("alias ", line, false, false);
     return line;
 }
